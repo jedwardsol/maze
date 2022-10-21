a0a925248564ecb45345594cdf6949bf7d109d5b
Jonathan Edwards <github@jonstuff.com>
2022-10-20 23:24:58 -0700

    maze

--- maze.cpp
+++ maze.cpp
@@ -1,7 +1,9 @@
-#include <stack>
+﻿#include <stack>
 #include <tuple>
 #include <array>
 #include <random>
+#include <iostream>
+#include <windows.h>
 
 enum class Cell : uint8_t
 {
@@ -11,6 +13,28 @@ enum class Cell : uint8_t
     down =8, 
     visited=16
 };
+
+
+std::array<char8_t const *,16> glyphs=
+{
+    u8" ",      //  0000
+    u8"╡",      //  0001
+    u8"╞",      //  0010
+    u8"═",      //  0011
+    u8"╨",      //  0100
+    u8"╝",      //  0101
+    u8"╚",      //  0110
+    u8"╩",      //  0111
+    u8"╥",      //  1000
+    u8"╗",      //  1001
+    u8"╔",      //  1010
+    u8"╦",      //  1011
+    u8"║",      //  1100
+    u8"╣",      //  1101
+    u8"╠",      //  1110
+    u8"╬",      //  1111
+};
+
 
 enum class Direction : uint8_t
 {
@@ -40,16 +64,21 @@ struct Pos
     int col;
 };
 
-template <size_t H, size_t W>
 class Grid
 {
 public:
-    constexpr auto height() const
+
+    Grid(size_t H, size_t W) : H{H}, W{W}
+    {
+        grid.resize(H*W);
+    }
+
+    auto height() const
     {
         return H;
     }
 
-    constexpr auto width() const
+    auto width() const
     {
         return W;
     }
@@ -61,7 +90,7 @@ public:
     
     bool visited(Pos const& pos) const
     {
-        return !(at(pos) & Cell::visited);
+        return (at(pos) & Cell::visited);
     }
 
     Cell const&at(Pos const& pos) const
@@ -83,12 +112,11 @@ public:
         }
     }
 
-
-
-
 private:
+    size_t                  H;
+    size_t                  W;
     int                     numVisitedCells{};
-    std::array<Cell,H*W>    grid{};
+    std::vector<Cell>       grid{};
 };
 
 
@@ -99,35 +127,107 @@ Direction chooseDirection(auto const &grid, Pos const &pos)
     std::vector<Direction> choices;
 
     if(    pos.col != 0
-        && !grid.visited(pos.row,pos.col-1))
+        && !grid.visited({pos.row,pos.col-1}))
     {
         choices.push_back(Direction::left);
     }
 
+    if(   pos.row != 0
+       && !grid.visited({pos.row-1, pos.col}))
+    {
+        choices.push_back(Direction::up);
+    }
+
+    if( pos.col != grid.width()-1
+        && !grid.visited({pos.row, pos.col+1}))
+    {
+        choices.push_back(Direction::right);
+    }
+
+    if (pos.row != grid.height() - 1
+        && !grid.visited({pos.row+1, pos.col}))
+    {
+        choices.push_back(Direction::down);
+    }
 
     if (choices.empty())
     {
         return Direction::none;
     }
-
+    else
+    {
+        return choices[ std::uniform_int_distribution<size_t>{0,choices.size()-1}(rng)];
+    }
 }
 
 
 int main()
 {
-    Grid<10,10>     grid;
+    SetConsoleOutputCP(CP_UTF8);
+    CONSOLE_SCREEN_BUFFER_INFO  screen{};
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&screen);
+
+    auto W = screen.srWindow.Right  - screen.srWindow.Left;
+    auto H = screen.srWindow.Bottom - screen.srWindow.Top;
+
+    Grid            grid(H-10,W-10);
     std::stack<Pos> path;
     Pos             walk{0,0};        
 
+    grid.visit(walk);
+    path.push(walk);
 
     while(grid.numVisited() < grid.height()*grid.width())
     {
-        grid.visit(walk);
-        path.push(walk);
+        Pos next{walk};
+        auto direction = chooseDirection(grid,walk);
 
+        switch(direction)
+        {
+        case Direction::none:
+            next=path.top();
+            path.pop();
+            break;
 
+        case Direction::right:
+            next.col++;
+            grid.at(walk) |= Cell::right;
+            grid.at(next) |= Cell::left;
+            path.push(next);
+            break;
 
+        case Direction::left:
+            next.col--;
+            grid.at(walk) |= Cell::left;
+            grid.at(next) |= Cell::right;
+            path.push(next);
+            break;
+
+        case Direction::down:
+            next.row++;
+            grid.at(walk) |= Cell::down;
+            grid.at(next) |= Cell::up;
+            path.push(next);
+            break;
+
+        case Direction::up:
+            next.row--;
+            grid.at(walk) |= Cell::up;
+            grid.at(next) |= Cell::down;
+            path.push(next);
+            break;
+        }
+
+        grid.visit(next);
+        walk = next;
     }
 
-
+    for(int row=0;row<grid.height();row++)
+    {
+        for (int col = 0; col < grid.width(); col++)
+        {
+            std::cout << reinterpret_cast<char const*>(glyphs[ grid.at({row,col}) & Cell{0b1111}]);
+        }
+        std::cout << "\n";
+    }
 }
